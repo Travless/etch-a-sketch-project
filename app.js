1237a6857cdc7aba5c8e14389523d3f180ad7d63
Travless <travis.white0101@gmail.com>
2022-09-23 16:25:20 -0400

    add hover event to grid of divs that changes background color to yello

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const boxContainer = document.querySelector('#box-container');
 let rowContainer = document.createElement('div');
 let row = document.createElement('div');
+let rowHoverColor = document.getElementById('#color-box');
 boxContainer.style.display = 'inline-flex';
+
+var mouseOverFunction = function() {
+    this.style.backgroundColor = 'yellow';
+};
+
+// rowHoverColor.onmouseover = mouseOverFunction;
+
+// rowHoverColor.addEventListener('mouseover', function(e) {
+//     row.style.backgroundColor = 'yellow';
+// })
+
+
+
+
+
+// row.addEventListener('mouseover', functtion(e) {
+//     rowHoverColor = row.style.backgroundColor = 'yellow'
+// })
 
 
 function makeRows(colQuantity, rowQuantity) {
@@ -12,11 +31,17 @@ function makeRows(colQuantity, rowQuantity) {
         for (let j = 0; j < rowQuantity; j++) {
             row = document.createElement('div');
             row.classList.add('box');
-            row.textContent = 'test';
+            row.setAttribute('id', 'color-box');
+            // row.textContent = 'test';
+            // row.style.backgroundColor = 'black';
             rowContainer.appendChild(row);
         };
     }
 };
+
+// function changeColor() {
+//     row.onmouseover = function(e)
+// }
 
 
 makeRows(36, 36);
