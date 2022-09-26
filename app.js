c629d62305682849e219db9bd7eb8f38a7fb9f7b
Travless <travis.white0101@gmail.com>
2022-09-26 18:55:01 -0400

    fix git error in previous commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let box;
 let numOfSquares;
 const boxContainer = document.querySelector('#box-container');
@@ -18,28 +17,6 @@ function numGridInput () {
 function makeGrid(numOfSquares) {
     for(let i = 0; i < numOfSquares; i++) {
         let colContainer = document.createElement('div');
-=======
-let numOfSquares;
-
-function numGridInput () {
-    numOfSquares = Number(prompt('How many squares would you like on each side?'));
-    if (numOfSquares <= 36 && numOfSquares >= 10) {
-        return numOfSquares;
-    } else {
-        alert('Invalid input, please enter a number of at least 10 or higher.');
-    }
-}
-
-const boxContainer = document.querySelector('#box-container');
-let colContainer = document.createElement('div');
-let box;
-boxContainer.style.display = 'flex';
-
-
-function makeGrid(numOfSquares) {
-    for(let i = 0; i < numOfSquares; i++) {
-        colContainer = document.createElement('div');
->>>>>>> e4f8781e6db9a3a469027d9f54472aa72e147d06
         colContainer.classList.add('container');
         boxContainer.appendChild(colContainer);
         for (let j = 0; j < numOfSquares; j++) {
@@ -50,7 +27,6 @@ function makeGrid(numOfSquares) {
     };
 };
 
-<<<<<<< HEAD
 numGridInput();
 makeGrid(numOfSquares);
 
@@ -67,15 +43,3 @@ refreshBtn.addEventListener('click', function(event) {
     location.reload();
     return false;
 })
-=======
-
-makeGrid(numOfSquares);
-
-const boxColor = document.getElementsByClassName('single');
-
-for (let i = 0; i < boxColor.length; i++) {
-    boxColor[i].addEventListener('mouseover', function(event) {
-        event.target.style.backgroundColor = 'black';
-    })
-}
->>>>>>> e4f8781e6db9a3a469027d9f54472aa72e147d06
